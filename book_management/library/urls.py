6f1b2af65fbe85a14a2c47db88061ed7c80284a5
Ari <aritheriver@gmail.com>
2024-03-27 16:33:38 -0700

    lil change

--- book_management/library/urls.py
+++ book_management/library/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('books/', views.book_list, name='book_list'),
     path('books/<int:book_id>/', views.book_detail, name='book_detail'),
     path('authors/', views.author_list, name='author_list'),
-    path('authors/<int:author_id>/', views.author_details, name='author_details'),
+    path('authors/<int:author_id>/', views.author_details, name='author_detail'),
 ]
